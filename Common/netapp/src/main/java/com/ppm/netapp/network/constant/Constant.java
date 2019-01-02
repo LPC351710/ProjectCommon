dd0040dc0c303d037d79d3598ce11bea1c434154
吕鹏程 <332033314@qq.com>
2019-01-02 18:54:24 +0800

    up

---
+++ Common/netapp/src/main/java/com/ppm/netapp/network/constant/Constant.java
@@ -0,0 +1,12 @@
+package com.ppm.netapp.network.constant;
+
+public final class Constant {
+
+    public static final String BASE_URL = "http://api.juheapi.com/japi/";
+
+    public static final String GET_EVENT_LIST = BASE_URL + "toh";
+
+    public static final String GET_EVENT_DETAILS = BASE_URL + "http://api.juheapi.com/japi";
+
+    public static final String KEY = "0dbb338d2f0bf8392d1ff773df23a555";
+}
--- Common/netapp/src/main/java/com/ppm/netapp/presenter/HisEventPresenter.java
+++ Common/netapp/src/main/java/com/ppm/netapp/presenter/HisEventPresenter.java
@@ -3,6 +3,7 @@ package com.ppm.netapp.presenter;
 import com.ppm.netapp.model.HisEvent;
 import com.ppm.netapp.network.HttpUtils;
 import com.ppm.netapp.network.callback.JsonCallBack;
+import com.ppm.netapp.network.constant.Constant;
 import com.ppm.netapp.view.IHisEventView;
 import com.ppm.ppcomon.base.presenter.BasePresenter;
 
@@ -13,12 +14,10 @@ public class HisEventPresenter extends BasePresenter<IHisEventView> {
 
     public void getHisEventList() {
         HttpUtils httpUtils = new HttpUtils();
-        Map<String, String> params = new HashMap<>();
-        params.put("key","0dbb338d2f0bf8392d1ff773df23a555");
-        params.put("v","1.0");
-        params.put("month","1");
-        params.put("day","1");
-        httpUtils.sendGetRequest("http://api.juheapi.com/japi/toh", params, new JsonCallBack<HisEvent>() {
+        Map<String, String> params = getCommonParams();
+        params.put("month", "1");
+        params.put("day", "1");
+        httpUtils.sendGetRequest(Constant.GET_EVENT_LIST, params, new JsonCallBack<HisEvent>() {
             @Override
             public void success(HisEvent event) {
                 if (event != null) {
@@ -31,5 +30,12 @@ public class HisEventPresenter extends BasePresenter<IHisEventView> {
 
             }
         });
+    }
+
+    protected Map<String, String> getCommonParams() {
+        Map<String, String> params = new HashMap<>();
+        params.put("key", Constant.KEY);
+        params.put("v", "1.0");
+        return params;
     }
 }
