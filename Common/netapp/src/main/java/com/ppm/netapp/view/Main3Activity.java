1d1f03d5a0f9157dd5f8d3ad8118167b8470632c
吕鹏程 <332033314@qq.com>
2019-04-26 18:12:31 +0800

    up

---
+++ Common/netapp/src/main/java/com/ppm/netapp/view/Main3Activity.java
@@ -0,0 +1,53 @@
+package com.ppm.netapp.view;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.ppm.netapp.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        WebView webView = findViewById(R.id.webview);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        webView.getSettings().setDefaultTextEncodingName("UTF-8");
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        String ua = webView.getSettings().getUserAgentString();
+        webView.getSettings().setGeolocationEnabled(true);
+        webView.getSettings().setUserAgentString(ua + "; rrdAdvance");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+
+        // 当安卓版本高于或等于17时，调用js播放音乐
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        }
+
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+
+//        webView.setWebChromeClient(new WebChromeClient());
+//        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://hnym.wxrrd.com/guider/apply?&token=8c52374a625e449fb97b48fafa8370a5");
+    }
+
+
+}
